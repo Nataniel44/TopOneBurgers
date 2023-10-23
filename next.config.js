5aab718f0e7cb99d8e5db446c9ba09347b7b02b8
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-10-23 15:42:35 -0300

    first commit1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+  // Configuración de rutas para el enrutamiento de Next.js
+  async rewrites() {
+    return [
+      {
+        source: "/old-route",
+        destination: "/new-route",
+      },
+    ];
+  },
 
-module.exports = nextConfig
+  // Configuración de redireccionamiento de rutas
+  async redirects() {
+    return [
+      {
+        source: "/about-us",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
+
+  // Configuración de variables de entorno públicas
+  env: {
+    API_URL: process.env.API_URL,
+  },
+
+  // Otras configuraciones de Next.js
+  // ...
+};
