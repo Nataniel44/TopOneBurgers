4b0ae7d76086bb5930199f880cd572b0ac6c7b81
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-10-23 21:12:47 -0300

    first commit1

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = {
-  // Configura el basePath si estás utilizando una ruta base en GitHub Pages
-  basePath: "/TopOneBurgers",
-
-  // Configura otras opciones, como webpack, redirecciones, etc.
-  // Ejemplo:
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   // Haz configuraciones específicas de Webpack aquí
-  //   return config;
-  // },
-
-  // Agrega encabezados de seguridad a tus respuestas
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
-  },
-
-  // Puedes configurar otras opciones según tus necesidades
-};
+module.exports = nextConfig;
