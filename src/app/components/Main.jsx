39e3cc26a738da1a70c7851909490413befcbade
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-10-23 21:24:27 -0300

    first commit1

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Header from "./Header";
+import Image from "next/image";
 const Main = () => {
   return (
     <>
@@ -13,15 +14,15 @@ const Main = () => {
           className="flex flex-col items-center gap-5"
         >
           <div className="flex flex-col justify-center items-center gap-5">
-            <img
+            <Image
               src="/img-redux/33.png"
-              className="rounded-full w-48  md:w-2/12 border border-yellow-400 border-4 shadow-md"
+              className="rounded-full w-48  md:w-2/12  border-yellow-400 border-4 shadow-md"
               alt=""
             />
 
             <div className="flex items-center">
               <h1 className=" text-4xl md:text-5xl text-center font-normal text">
-                Top One Burger's
+                Top One Burgers
               </h1>
             </div>
           </div>
