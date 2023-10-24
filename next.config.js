7e9909550c6809ef767f35adb55ab887087fdbf1
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-10-23 21:32:53 -0300

    first commit1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  // Otras configuraciones...
+  target: "experimental-serverless-trace",
+  reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    loader: "imgix",
+    path: "",
+  },
+  // Agrega esta línea para habilitar la exportación estática
+  output: "out",
+};
