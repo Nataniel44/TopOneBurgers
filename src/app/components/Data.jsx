dea79ae3d0e5d6bb5e5f34dad2ec497d92328335
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-11-04 21:09:52 -0300

    Actualizar Data.jsx

--- src/app/components/Data.jsx
+++ src/app/components/Data.jsx
@@ -1,14 +1,14 @@
 const hamburguesasData = [
   {
     nombre: "Classic",
-    precio: 1400,
+    precio: 1500,
     imagen: "../img-redux/classic.jpg",
     ingredientes:
       "Pan, medallon de carne, queso, jamón, huevo, tomate, lechuga, aderezos.",
   },
   {
     nombre: "Argenta",
-    precio: 1600,
+    precio: 1800,
     imagen: "../img-redux/argen.jpg",
     ingredientes:
       "Pan, medallon de carne, huevo, queso provoleta, salsa criolla, aderezos.",
@@ -16,27 +16,27 @@ const hamburguesasData = [
 
   {
     nombre: "Rúcula y parmesano",
-    precio: 1600,
+    precio: 1800,
     imagen: "../img-redux/rucula.jpg",
     ingredientes:
       "Pan, medallon de carne, queso, jamón, huevo, tomate, lechuga, aderezos.",
   },
   {
     nombre: "Chesse",
-    precio: 1000,
+    precio: 1200,
     imagen: "../img-redux/cheese.jpg",
     ingredientes: "Pan, medallon de carne, queso cheddar.",
   },
   {
     nombre: "Top One",
-    precio: 1600,
+    precio: 1800,
     imagen: "../img-redux/topone.jpg",
     ingredientes:
       "Pan, medallon de carne, queso, jamón, huevo, tomate, lechuga, aderezos.",
   },
   {
     nombre: "Hulk",
-    precio: 2100,
+    precio: 2300,
     imagen: "../img-redux/hulk1.jpg",
     ingredientes:
       "Pan, medallon de carne, queso, jamón, huevo, tomate, lechuga, aderezos.",
